3724e8aea5526350e195dc6eb0960661b577ae2a
Anderson Mesquita <andersonvom@gmail.com>
2012-11-04 10:48:02 -0500

    Initialized variables

--- a-star/a-star.rb
+++ a-star/a-star.rb
@@ -7,6 +7,16 @@ module Algorithms
     #     [.]     for available paths
     #     [_-|]   for blocked paths
     def solve(area, start, goal, heuristic)
+      closed_set = {}         # visited nodes
+      open_set = {start: 1}   # possible nodes to visit
+      came_from = {}          # used to reconstruct path from `goal` to `start`
+
+      f_score = Hash.new {0}  # Cost from `start` to `goal` using `heuristic`
+      g_score = Hash.new {0}  # Best cost coming from `start`
+      g_score[start] = 0
+
+      while !open_set.empty?
+      end
     end
     
   end
