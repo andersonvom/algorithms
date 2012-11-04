6bf2708add2ef4efe368cf2bc140778873773234
Anderson Mesquita <andersonvom@gmail.com>
2012-11-04 09:59:52 -0500

    Added comments to A*

--- a-star/a-star.rb
+++ a-star/a-star.rb
@@ -2,8 +2,11 @@ module Algorithms
 
   module AStar
   
+    # Finds a path in `area` between `start` and `goal` using the `heuristic` function
+    # `area` is a 2-dimentional array containing:
+    #     [.]     for available paths
+    #     [_-|]   for blocked paths
     def solve(area, start, goal, heuristic)
-      
     end
     
   end
