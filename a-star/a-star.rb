917a0bccf571b07e1ac0f85fc207ff6e2d5907fc
Anderson Mesquita <andersonvom@gmail.com>
2012-11-04 11:02:55 -0500

    Refactored A* to use Sets instead of Hashes

--- a-star/a-star.rb
+++ a-star/a-star.rb
@@ -1,18 +1,19 @@
 module Algorithms
 
   module AStar
+    require 'set'
   
     # Finds a path in `area` between `start` and `goal` using the `heuristic` function
     # `area` is a 2-dimentional array containing:
     #     [.]     for available paths
     #     [_-|]   for blocked paths
     def solve(area, start, goal, heuristic)
-      closed_set = {}         # visited nodes
-      open_set = {start: 1}   # possible nodes to visit
-      came_from = {}          # used to reconstruct path from `goal` to `start`
+      closed_set = Set.new        # visited nodes
+      open_set = Set.new [start]  # possible nodes to visit
+      came_from = {}              # used to reconstruct path from `goal` to `start`
 
-      f_score = Hash.new {0}  # Cost from `start` to `goal` using `heuristic`
-      g_score = Hash.new {0}  # Best cost coming from `start`
+      f_score = Hash.new {0}      # Cost from `start` to `goal` using `heuristic`
+      g_score = Hash.new {0}      # Best cost coming from `start`
       g_score[start] = 0
 
       while !open_set.empty?
